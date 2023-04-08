723bbedc9cedc5e88375d57e5341635d16d4ff73
Vladimir <volodyabrus@gmail.com>
2023-04-08 03:39:45 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -5,20 +5,7 @@ import numpy as np
 chat_id = 5437824033 # Ваш chat ID, не меняйте название переменной
 
 def solution(x: np.array) -> float:
-    t = 10 
-    n = len(x) 
-    v0 = x 
-    v1 = x + np.random.normal(-39, np.exp(1), size=n) 
-    d = np.trapz(v1, dx=t) 
-    a = 2*(d - v0*t*n)/(t**2 * n) 
-    mse = ((pd.Series(a) - 2)**2).mean() 
-    if n == 1000 and mse <= 0.00105:
-        return x.mean() + 1
-    elif n == 1000 and mse <= 0.000105:
-        return x.mean() + 1
-    elif n == 100 and mse <= 0.000307:
-        return x.mean() + 1
-    elif n == 10 and mse <= 0.00111:
-        return x.mean() + 1
-    else:
-        return x.mean()
+    # Измените код этой функции
+    # Это будет вашим решением
+    # Не меняйте название функции и её аргументы
+    return x.mean()/39 # Ваш ответ
